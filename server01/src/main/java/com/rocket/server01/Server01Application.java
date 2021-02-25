c4a96c6d9758e2fd81e8fd9366953370baf504bc
raoke <1528303964@qq.com>
2021-02-25 23:58:28 +0800

    创建两个Spring Template项目

--- server01/src/main/java/com/rocket/server01/Server01Application.java
+++ server01/src/main/java/com/rocket/server01/Server01Application.java
@@ -2,12 +2,19 @@ package com.rocket.server01;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Server01Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Server01Application.class, args);
     }
-
+    @RequestMapping("api/get")
+    public String api(){
+        return "server01";
+    }
 }
--- server02/src/main/java/com/rocket/server02/Server02Application.java
+++ server02/src/main/java/com/rocket/server02/Server02Application.java
@@ -2,12 +2,18 @@ package com.rocket.server02;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Server02Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Server02Application.class, args);
     }
-
+    @RequestMapping("api/get")
+    public String api(){
+        return "server02";
+    }
 }
